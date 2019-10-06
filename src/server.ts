60bd65be15e4a716e7d72d33bee01c5cb62e927f
haojie06 <haojie06@qq.com>
2019-10-06 09:33:14 +0800

    test

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,12 @@ export class WSServer extends EventEmitter{
         this._socket = new Server({port:port});
         //当有客户端连接时回调
         console.log("WS开始监听" + port);
-        this._socket.on("connection",(socket,request)=>{
+        this._socket.on("connection",socket => {
             console.log("客户端连接");
             //当socket收到信息时回调
-            socket.on("message", function(message) {
+            socket.on("message", message => {
+                console.log("接收到信息");
+                /*
                 console.log(socket.url);
                 let data = JSON.parse(message as string);
                 let msgPurpose = data.header.messagePurpose;
@@ -24,10 +26,10 @@ export class WSServer extends EventEmitter{
                 else{
                     console.log(data.body.eventName);
                 }
+                */
             });
 
             //socket.on("close", () => {console.log("客户端断开连接")});
-        
         });
     }
 }
